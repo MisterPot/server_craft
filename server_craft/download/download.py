aff2d79b6d113607845403d64b75fc77a75b5221
MisterPot <english.man.spy@gmail.com>
2021-11-06 21:18:44 +0200

    Basicly functional of `chester` added

--- server_craft/download/download.py
+++ server_craft/download/download.py
@@ -5,106 +5,115 @@ import socket
 import server_craft
 
 versions = {
-	'1.17.1': 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
-	'1.16.5': 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
-	'1.15.2': 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
-	'1.14.4': 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
-	'1.13.2': 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
-	'1.12.2': 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+    '1.17.1': 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
+    '1.16.5': 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    '1.15.2': 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    '1.14.4': 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
+    '1.13.2': 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
+    '1.12.2': 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+}
+
+to_java_versions = {
+    '1.17.1': '17.',
+    '1.16.5': '1.8',
+    '1.15.2': '1.8',
+    '1.14.4': '1.8',
+    '1.13.2': '1.7',
+    '1.12.2': '1.7',
 }
 
 
 def get_host_ip():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.connect(("8.8.8.8", 80))
-	return sock.getsockname()[0]
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect(("8.8.8.8", 80))
+    return sock.getsockname()[0]
 
 
 class Download(object):
 
-	def __init__(self, version):
-		try:
-			server_craft.get_ok(version)
+    def __init__(self, version):
+        try:
+            server_craft.get_ok(version)
 
-		except server_craft.JavaNotFound:
-			server_craft.check_permission()
-			server_craft.java_download(version)
-			try:
-				server_craft.get_ok(version)
-			except server_craft.JavaNotFound:
-				print('Reboot your computer.')
-				print('JAVA_HOME variable not commited')
+        except server_craft.JavaNotFound:
+            server_craft.check_permission()
+            java_ver = to_java_versions[version]
+            server_craft.java_download(java_ver)
 
-		self.version = version
-		self.url = versions[version]
-		self.jar_file = None
+        self.version = version
+        self.url = versions[version]
+        self.jar_file = None
 
-	def download(self, server_dir='.'):
-		print('Downloading....')
-		try:
-			os.chdir(server_dir)
+    def download(self, server_dir='.'):
+        print('Downloading....')
+        try:
+            os.chdir(server_dir)
 
-		except FileNotFoundError:
-			print('Wrong path to server directory!')
+        except FileNotFoundError:
+            print('Wrong path to server directory!')
 
-		try:
-			s = rq.Session()
-			jar = s.get(self.url).content
+        path = server_craft.check_version(self.version)
 
-		except Exception:
-			print('Download not successful.')
-			print('Check your internet connection ! (may you use proxy ?)')
-			return 0
+        if path:
+            server_craft.copy_from_storage(self.version)
+        else:
+            try:
+                s = rq.Session()
+                jar_request = s.get(self.url)
+                path = server_craft.save_server_jar(self.version, jar_request)
+                server_craft.copy_from_storage(self.version)
 
-		with open('server.jar', 'wb') as file:
-			file.write(jar)
+            except Exception:
+                print('Download not successful.')
+                print('Check your internet connection ! (may you use proxy ?)')
+                return 0
 
-		self.jar_file = os.path.join(os.getcwd(), 'server.jar')
+        self.jar_file = path
 
-		print('Download successful!')
-		print('Unpacking....')
+        print('Download successful!')
+        print('Unpacking....')
 
-		self.__unpack()
-		self.__change_properties()
+        self.__unpack()
+        self.__change_properties()
 
-	def __change_properties(self):
+    def __change_properties(self):
 
-		print('Prepare file "server.properties"')
-		with open('server.properties', 'r') as file:
-			out = file.readlines()
+        print('Prepare file "server.properties"')
+        with open('server.properties', 'r') as file:
+            out = file.readlines()
 
-		out.insert(2, '[server]')
+        out.insert(2, '[server]')
 
-		with open('server.properties', 'w') as file:
-			file.writelines(out)
+        with open('server.properties', 'w') as file:
+            file.writelines(out)
 
-		host_ip = get_host_ip()
-		free_port = server_craft.get_free_port()
-		properties = server_craft.Properties('server.properties')
-		properties.set('online-mode', 'false')
-		properties.set('server-ip', host_ip)
-		properties.set('difficulty', 'normal')
-		properties.set('query.port', free_port)
-		properties.set('server-port', free_port)
-		properties.confirm()
-		print('Seted online-mode = false')
-		print(f'Seted server-ip = {host_ip}')
-		print('Now use Server object to run this server')
+        host_ip = get_host_ip()
+        free_port = server_craft.get_free_port()
+        properties = server_craft.Properties('server.properties')
+        properties.set('online-mode', 'false')
+        properties.set('server-ip', host_ip)
+        properties.set('difficulty', 'normal')
+        properties.set('query.port', free_port)
+        properties.set('server-port', free_port)
+        properties.confirm()
+        print('Seted online-mode = false')
+        print(f'Seted server-ip = {host_ip}')
+        print('Now use Server object to run this server')
 
-	def __unpack(self):
+    def __unpack(self):
 
-		proc = subprocess.run(['java', '-jar', self.jar_file],
-		 					  stderr=subprocess.DEVNULL,
-		 					  stdout=subprocess.DEVNULL)
+        proc = subprocess.run(['java', '-jar', self.jar_file],
+                              stderr=subprocess.DEVNULL,
+                              stdout=subprocess.DEVNULL)
 
-		with open('eula.txt', 'r') as file:
-			out = file.readlines()
+        with open('eula.txt', 'r') as file:
+            out = file.readlines()
 
-		confirm = out[len(out) - 1].replace('false', 'true')
-		out[len(out) - 1] = confirm
+        confirm = out[len(out) - 1].replace('false', 'true')
+        out[len(out) - 1] = confirm
 
-		with open('eula.txt', 'w') as file:
-			file.writelines(out)
+        with open('eula.txt', 'w') as file:
+            file.writelines(out)
 
-		print('Unpacked successful!')
-		print('Eula is true')
+        print('Unpacked successful!')
+        print('Eula is true')

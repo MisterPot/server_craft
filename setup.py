accbfcb56722d48cc3d235aa98b7de583c8b14e7
MisterPot <english.man.spy@gmail.com>
2021-11-09 22:31:48 +0200

    Added exchanger to scripts

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='server_craft',
-	version='1.2',
-	packages=find_packages(),
-	install_requires=[
-		'requests==2.25.1',
-	],
-	include_package_data=True,
-	package_data={
-		"": ['*.conf', '*.json'],
-	},
-	exclude_package_data={"": ["README.md"]},
-	entry_points={'console_scripts': ['craft=server_craft.scripts.craft:main']}	
+    name='server_craft',
+    version='1.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.25.1',
+    ],
+    include_package_data=True,
+    package_data={
+        "": ['*.conf', '*.json'],
+    },
+    exclude_package_data={"": ["README.md"]},
+    entry_points={'console_scripts': ['craft=server_craft.scripts.craft:main',
+                                      'exw=server_craft.scripts.exchange:main']}
 )
